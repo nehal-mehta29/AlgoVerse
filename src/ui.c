1894794454f8f91d0ec709d4ea9b49196bc4e7d7
Nehal Mehta <nehal29096@gmail.com>
2026-07-30 11:46:09 +0530

    ui updated

--- src/ui.c
+++ src/ui.c
@@ -12,10 +12,10 @@ void splashScreen(void){
 
     printf("\n");
     printf("     _    _     ____   ___ __     __ _____ ____  ____ _____\n");
-    printf("    / \\\\  | |   / ___| / _ \\\\\\ \\   / /| ____|  _ \\\\/ ___| ____|\n");
-    printf("   / _ \\\\ | |  | |  _ | | | |\\\\ \\ / / |  _| | |_) \\\\___ \\\\|  _|\n");
-    printf("  / ___ \\\\| |__| |_| || |_| | \\\\ V /  | |___|  _ < ___) | |___\n");
-    printf(" /_/   \\\\_\\\\_____\\\\____| \\\\___/   \\\\_/   |_____|_| \\\\_\\\\____/|_____|\n");
+    printf("    / \\  | |   / ___| / _ \\\\ \\   / /| ____|  _ \\/ ___| ____|\n");
+    printf("   / _ \\ | |  | |  _ | | | |\\ \\ / / |  _| | |_) \\___ \\|  _|\n");
+    printf("  / ___ \\| |__| |_| || |_| | \\ V /  | |___|  _ < ___) | |___\n");
+    printf(" /_/   \\_\\_____\\____| \\___/   \\_/   |_____|_| \\_\\____/|_____|\n");
 
     printf(COLOR_RESET);
 
@@ -28,7 +28,7 @@ void splashScreen(void){
     printf("\n");
 
     printf(COLOR_ACCENT);
-    printf("                  Visualize • Analyze • Understand\n");
+    printf("                  Visualize * Analyze * Understand\n");
     printf(COLOR_RESET);
 
     printf("\n\n");
