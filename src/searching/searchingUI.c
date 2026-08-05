83cec7ab19aecce20e667af12cd06f7b6d0f8b4f
Nehal Mehta <nehal29096@gmail.com>
2026-08-05 11:07:27 +0530

    initial commit

---
+++ src/searching/searchingUI.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+
+#include "searching/searchingUI.h"
+
+#include "colors.h"
+#include "utils.h"
+
+/*==========================================================
+                Internal Helper Functions
+==========================================================*/
+
+/* Will add helper functions here */
+
+/*==========================================================
+                Original Array Screen
+==========================================================*/
+
+void printOriginalArrayScreen(const int arr[],
+                              int size){
+
+}
+
+/*==========================================================
+                Sorted Array Screen
+==========================================================*/
+
+void printSortedArrayScreen(const int arr[],
+                            int size){
+
+}
+
+/*==========================================================
+                Search Header
+==========================================================*/
+
+void printSearchHeader(const char *algorithm,
+                       int step){
+
+}
+
+/*==========================================================
+                Array Renderer
+==========================================================*/
+
+void drawArray(const int arr[],
+               int size,
+               int current,
+               int secondary,
+               int start,
+               int end,
+               int mode){
+
+}
+
+/*==========================================================
+                Search Information
+==========================================================*/
+
+void printComparison(int first,
+                     int second,
+                     const char *relation){
+
+}
+
+void printSearchDirection(const char *direction){
+
+}
+
+void printCurrentRange(int start,
+                       int end){
+
+}
+
+void printCurrentIndex(int index){
+
+}
+
+/*==========================================================
+                Final Result
+==========================================================*/
+
+void printElementFound(int index){
+
+}
+
+void printElementNotFound(void){
+
+}
